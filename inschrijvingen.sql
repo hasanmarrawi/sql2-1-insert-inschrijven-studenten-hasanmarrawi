89357008afca692653d4f2e207b655975f9b88ae
hasanmarrawi <48281963+hasanmarrawi@users.noreply.github.com>
2019-05-10 15:29:56 +0200

    Koppelen student klas
    Koppelen student klas

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,6 +1,6 @@
 USE `lo8e_sql2`;
-INSERT INTO studenten (ov_nummer, achternaam, tussenvoegsel, voornaam, geboortedatum, postcode, plaats, gewicht, lengte, inschrijvings_datum)
-VALUES  (90000 ,'Hasan',NULL,'Marrawi','1999-05-15','2347 SM','LEIDEN', 62.5, 1.86, '2018-09-11'),
-		(90001 ,'Timo',NULL,'Schouten','2002-03-19','9134 PR','KATEIJK', 64.2, 1.86, '2018-08-04'),
-        (90002 ,'Kristian','de','Ruiten','2001-11-25','8343 IF','LEIDEN', 59.6, 1.86, '2018-09-23'),
-        (90003 ,'Tolga',NULL,'Jeremy','2002-05-08','3782 FL','LEIDEN', 61.8, 1.86, '2018-09-27');
+INSERT INTO klassen_studenten (ov_nummer, klas_code)
+VALUES  (90000 ,'lo8e-amo1'),
+		(90001 ,'lo8e-amo3'),
+        (90002 ,'lo8e-amo4'),
+        (90003 ,'lo8e-amo2');
