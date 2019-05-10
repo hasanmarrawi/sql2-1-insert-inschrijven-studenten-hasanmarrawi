854dd8d9d5e913809735cf160cf4640c18561d5e
hasanmarrawi <48281963+hasanmarrawi@users.noreply.github.com>
2019-05-10 15:23:48 +0200

    Update inschrijvingen.sql
    Studenten aanmaken

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,5 +1,6 @@
 USE `lo8e_sql2`;
-INSERT INTO klassen (klas_code,slb_code,cohort,opleiding_code)
-VALUES  ('lo9e-amo1','vlt03', 2019, 'amo'), 
-		('lo9e-amo2','bkr02', 2019, 'amo'), 
-		('lo9e-amo3','rs001', 2019, 'amo');
+INSERT INTO studenten (ov_nummer, achternaam, tussenvoegsel, voornaam, geboortedatum, postcode, plaats, gewicht, lengte, inschrijvings_datum)
+VALUES  (90000 ,'Hasan',NULL,'Marrawi','1999-05-15','2347 SM','LEIDEN', 62.5, 1.86, '2018-09-11'),
+		(90001 ,'Timo',NULL,'Schouten','2002-03-19','9134 PR','KATEIJK', 64.2, 1.86, '2018-08-04'),
+        (90002 ,'Kristian','de','Ruiten','2001-11-25','8343 IF','LEIDEN', 59.6, 1.86, '2018-09-23'),
+        (90003 ,'Tolga',NULL,'Jeremy','2002-05-08','3782 FL','LEIDEN', 61.8, 1.86, '2018-09-27');
